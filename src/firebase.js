7250c05733df4b53ada84ba33af6de0cfa62a267
Muhammad Adeel <muhammaddadeel90@gmail.com>
2024-07-12 00:15:01 +0500

    firebase authentication protected

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBJQh1NK06KaAZZsuWpLuYnpVurDZv52Rk",
-    authDomain: "responder--response-system.firebaseapp.com",
-    databaseURL: "https://responder--response-system-default-rtdb.firebaseio.com",
-    projectId: "responder--response-system",
-    storageBucket: "responder--response-system.appspot.com",
-    messagingSenderId: "478410201618",
-    appId: "1:478410201618:web:eb235e53d817eedd8e96cb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 // Initialize Firebase
